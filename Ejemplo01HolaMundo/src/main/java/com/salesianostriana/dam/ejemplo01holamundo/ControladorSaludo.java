85a915f75800a1b42671e25b8150e67722bc0886
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-04-22 11:55:45 +0200

    asdfasd

--- Ejemplo01HolaMundo/src/main/java/com/salesianostriana/dam/ejemplo01holamundo/ControladorSaludo.java
+++ Ejemplo01HolaMundo/src/main/java/com/salesianostriana/dam/ejemplo01holamundo/ControladorSaludo.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.dam.ejemplo01holamundo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,4 +15,19 @@ public class ControladorSaludo {
 		return "index";
 	}
 
+	@GetMapping("/saludo2")
+	public String welcome2(Model model) {
+		model.addAttribute("persona", new Persona
+
+		("Ángel", "Naranjo González"));
+		return "SaludoPersonalizado";
+	}
+
+	@GetMapping("/saludo3")
+	public String welcome3(Model model) {
+		model.addAttribute("saludo", "Hola Mundo");
+		model.addAttribute("mensaje", "¡Se me está haciendo largo el proyecto final!");
+		model.addAttribute("url", "https://triana.salesianos.edu");
+		return "SaludoYEnlace";
+	}
 }
---
+++ Prueba01ManuelGomez/src/main/java/com/salesianostriana/dam/prueba01manuelgomez/Prueba01ManuelGomezApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.prueba01manuelgomez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba01ManuelGomezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba01ManuelGomezApplication.class, args);
+	}
+
+}
